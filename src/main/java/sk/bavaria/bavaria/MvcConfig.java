00d9ff15e9df7d1bc0be10505afac4fd670a628e
my <my@email.com>
2017-11-09 19:27:39 +0100

    routing updated

--- src/main/java/sk/bavaria/bavaria/MvcConfig.java
+++ src/main/java/sk/bavaria/bavaria/MvcConfig.java
@@ -3,8 +3,8 @@ package sk.bavaria.bavaria;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,20 +13,27 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableWebMvc
 @EnableSwagger2
 public class MvcConfig extends WebMvcConfigurerAdapter {
     public MvcConfig() {
-        super();
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/", "/resources/", "classpath:/other-resources/");
+        registry.addResourceHandler( "/static/**").addResourceLocations("/resources/public/static/");
+
         registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/resources/public/index.html");
+        registry.addViewController("/autoservice").setViewName("forward:/resources/public/index.html");
+        registry.addViewController("/galery").setViewName("forward:/resources/public/index.html");
+        registry.addViewController("/sales").setViewName("forward:/resources/public/index.html");
+        registry.addViewController("/contact").setViewName("forward:/resources/public/index.html");
+        registry.addViewController("/review").setViewName("forward:/resources/public/index.html");
+    }
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
